6fc331488106bf1820e652b1184cde8597876870
Cristian Mejia <crmejiam@unal.edu.co>
2021-01-28 12:49:06 -0500

    add explicit waits

--- src/page/product-added-modal.page.ts
+++ src/page/product-added-modal.page.ts
@@ -1,10 +1,11 @@
-import { $, ElementFinder } from 'protractor';
+import { $, browser, ElementFinder } from 'protractor';
 
 export class ProductAddedModalPage {
   private cartButton: ElementFinder;
 
   constructor () {
     this.cartButton = $('[style*="display: block;"] .button-container > a');
+    browser.manage().timeouts().implicitlyWait(1000);
   }
 
   public async addToCart(): Promise<void> {
--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,10 +1,11 @@
-import { $, ElementFinder } from 'protractor';
+import { $, browser, ElementFinder } from 'protractor';
 
 export class ProductListPage {
   private productMenu: ElementFinder;
 
   constructor () {
     this.productMenu = $('#center_column a.button.ajax_add_to_cart_button.btn.btn-default');
+    browser.manage().timeouts().implicitlyWait(5000);
   }
 
   public async goToProductMenu(): Promise<void> {
